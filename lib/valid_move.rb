26bdb58f57a06c7bc941b6557208b641d1c98ead
Gbemileke Onilude <lekeonilude@gmail.com>
2017-11-25 15:28:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(index)
-  
+  if position_taken? and index.between(0, 8)
+    true
+  else
+    false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
